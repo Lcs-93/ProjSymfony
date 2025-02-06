ff83bef7529c3c30627ff64ff875e71d06db33f8
lcs-93 <baillylucas04@gmail.com>
2025-02-06 17:38:15 +0100

    premier commit

--- src/Form/VehiculeType.php
+++ src/Form/VehiculeType.php
@@ -6,18 +6,36 @@ use App\Entity\Vehicule;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class VehiculeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('marque')
-            ->add('modele')
-            ->add('immatriculation')
-            ->add('prixJournalier')
-            ->add('disponible')
-        ;
+            ->add('marque', TextType::class, [
+                'label' => 'Marque',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('modele', TextType::class, [
+                'label' => 'Modèle',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('immatriculation', TextType::class, [
+                'label' => 'Immatriculation',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('prixJournalier', NumberType::class, [
+                'label' => 'Prix Journalier (€)',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('disponible', CheckboxType::class, [
+                'label' => 'Disponible',
+                'required' => false,
+                'attr' => ['class' => 'form-check-input'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Repository/VehiculeRepository.php
+++ src/Repository/VehiculeRepository.php
@@ -15,49 +15,32 @@ class VehiculeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Vehicule::class);
     }
+
+    /**
+     * Trouve les véhicules en fonction des filtres.
+     *
+     * @param array $filters Les critères de recherche (marque, prixMax, disponible)
+     * @return Vehicule[]
+     */
     public function findByFilters(array $filters): array
-{
-    $qb = $this->createQueryBuilder('v');
+    {
+        $qb = $this->createQueryBuilder('v');
 
-    if (!empty($filters['marque'])) {
-        $qb->andWhere('v.marque LIKE :marque')
-           ->setParameter('marque', '%' . $filters['marque'] . '%');
+        if (!empty($filters['marque'])) {
+            $qb->andWhere('v.marque LIKE :marque')
+               ->setParameter('marque', '%' . $filters['marque'] . '%');
+        }
+
+        if (!empty($filters['prixMax'])) {
+            $qb->andWhere('v.prixJournalier <= :prixMax')
+               ->setParameter('prixMax', $filters['prixMax']);
+        }
+
+        if (!empty($filters['disponible'])) {
+            $qb->andWhere('v.disponible = :disponible')
+               ->setParameter('disponible', true);
+        }
+
+        return $qb->getQuery()->getResult();
     }
-
-    if (!empty($filters['prixMax'])) {
-        $qb->andWhere('v.prixJournalier <= :prixMax')
-           ->setParameter('prixMax', $filters['prixMax']);
-    }
-
-    if (!empty($filters['disponible'])) {
-        $qb->andWhere('v.disponible = :disponible')
-           ->setParameter('disponible', true);
-    }
-
-    return $qb->getQuery()->getResult();
-}
-    //    /**
-    //     * @return Vehicule[] Returns an array of Vehicule objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('v')
-    //            ->andWhere('v.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('v.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Vehicule
-    //    {
-    //        return $this->createQueryBuilder('v')
-    //            ->andWhere('v.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
